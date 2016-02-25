20e8fc0f045a8005d15cce58296e7dcadbd35113
Uriel Miranda <urielnovoa@gmail.com>
2016-02-25 17:57:58 -0600

    Update

---
+++ public/javascripts/app.js
@@ -0,0 +1,47 @@
+(function(){
+'use strict';
+
+ angular.module("myapp",['ngResource'])
+.directive("cardCharacter", cardCharacter)
+.factory('starWarSearch',starWarSearch);
+
+ starWarSearch.$inject = ['$resource'];
+ function starWarSearch($resource){
+   return $resource('http://swapi.co/api/people/');
+ }
+
+cardCharacter.$inject = [];
+function cardCharacter(){
+  var directive = {
+    restrict:"EA",
+    templateUrl: "/partials/cards.html",
+    controller: cardController,
+    controllerAs: 'cardinfo',
+    binToController: true,
+    replace: true,
+
+  };
+  return directive;
+}
+
+cardController.$inject = ["starWarSearch"];
+function cardController(starWarSearch){
+  var cardinfo = this;
+
+  cardinfo.doSearch = doSearch;
+
+
+  function doSearch(){
+    console.log(cardinfo.name);
+    starWarSearch.get({'id': cardinfo.name})
+    .$promise
+      .then(function(response){
+        cardinfo.characters = response.results[0];
+        console.log(response.results[0]);
+      });
+  }
+}
+
+
+
+})();
